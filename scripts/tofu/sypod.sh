dda5843cac23d9cba231956e3a6b087c144e8439
ssangyeon <pingpinglee00@gmail.com>
2025-04-18 02:33:10 +0000

    234231111111

--- scripts/tofu/sypod.sh
+++ scripts/tofu/sypod.sh
@@ -3,33 +3,62 @@ MASTER_PORT=$((RANDOM % 50001 + 10000))
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 forget_losses=(
-    GA1+GD1
-    GA2+GD2
-    GA3+GD3
-    # NPO1+GD1
-    # NPO2+GD2
-    # NPO3+GD3
+    # GA1+GD1
+    # # GA2+GD1
+    # # GA3+GD1
+    GA1+GD3
+    GA2+GD3
+    # ##1
     # GA1+KL1
-    # GA2+KL2
-    # GA3+KL3
+    # # GA2+KL1
+    # # GA3+KL1
+    # GA1+KL3
+    # GA2+KL3
+    ##2
+    # NPO1+GD1
+    # # NPO2+GD1
+    # # NPO3+GD1
+    # NPO1+GD3
+    # NPO2+GD3
+    ##3
     # NPO1+KL1
-    # NPO2+KL2
-    # NPO3+KL3
-    # GA1
-    # GA2
-    # GA3
-    # NPO1
-    # NPO2
-    # NPO3
-    # IDK2
-    # IDK1
-    # IDK3
-    # IDK2+GD2
+    # # NPO2+KL1
+    # # NPO3+KL1
+    # NPO1+KL3
+    # NPO2+KL3
+    ##4
     # IDK1+GD1
-    # IDK3+GD3
-    # IDK2+KL2
+    # # IDK2+GD1
+    # # IDK3+GD1
+    # IDK1+GD3
+    # IDK2+GD3
+    ##5
     # IDK1+KL1
-    # IDK3+KL3
+    # # IDK2+KL1
+    # # IDK3+KL1
+    # IDK1+KL3
+    # IDK2+KL3
+    ##6
+    # SDK2+GD1
+    # SDK3+GD1
+    # SDK+GD1
+    # SDK2+KL1
+    # SDK3+KL1
+    # SDK+KL1
+
+    ##7
+    # GA2+GD1
+    # GA3+GD1
+    # GA2+KL1
+    # GA3+KL1
+    # NPO2+GD1
+    # NPO3+GD1
+    # NPO2+KL1
+    # NPO3+KL1
+    # IDK2+GD1
+    # IDK3+GD1
+    # IDK2+KL1
+    # IDK3+KL1
 )
 cuda_id=0
 
@@ -39,7 +68,6 @@ task_list=(1)
 learning_rates=(
     1e-5
 )
-task_list=(1)
 
 # pass to python script
 export TASK_LIST=$(IFS=,; echo "${task_list[*]}")
@@ -54,13 +82,11 @@ regularization_coeff=1.0
 
 save_checkpoint=false
 
-
 save_steps=last
 eval_steps=(last)
 
 num_epochss=(1 2 3 4 5)
 split=forget05
-
 for forget_loss in "${forget_losses[@]}"; do
     for num_epochs in "${num_epochss[@]}"; do
         for lr in "${learning_rates[@]}"; do
@@ -94,8 +120,8 @@ for forget_loss in "${forget_losses[@]}"; do
     done
 done
 
+# num_epochss=(1 2)
 # split=forget05
-# num_epochss=(1 2 3 4 5)
 # for forget_loss in "${forget_losses[@]}"; do
 #     for num_epochs in "${num_epochss[@]}"; do
 #         for lr in "${learning_rates[@]}"; do
@@ -117,48 +143,14 @@ done
 #                     eval_unlearn_step=$step \
 #                     $COMMON
 #             done
-#             # for step in "${eval_steps[@]}"; do
-#             #     CUDA_VISIBLE_DEVICES=$cuda_id torchrun --nproc_per_node=1 --master_port=$MASTER_PORT \
-#             #         eval2.py \
-#             #         --config-name=tofu.yaml \
-#             #         task_id=$task_id \
-#             #         eval_unlearn_step=$step \
-#             #         $COMMON
-#             # done
-#         done
-#     done
-# done
-
-# split=forget10
-# for forget_loss in "${forget_losses[@]}"; do
-#     for num_epochs in "${num_epochss[@]}"; do
-#         for lr in "${learning_rates[@]}"; do
-#             for task_id in "${task_list[@]}"; do
-#                 COMMON="use_LoRA=$use_LoRA forget_coeff=$forget_coeff regularization_coeff=$regularization_coeff lr=$lr split=$split forget_loss=$forget_loss num_epochs=$num_epochs \
-#                     mask=$mask fix_ref_model=$fix_ref_model save_root=$save_root save_checkpoint=$save_checkpoint model_path=$model_path"
-#                 CUDA_VISIBLE_DEVICES=$cuda_id torchrun --nproc_per_node=1 --master_port=$MASTER_PORT \
-#                     forget.py \
-#                     --config-name=tofu.yaml \
-#                     task_id=$task_id \
-#                     save_steps=$save_steps \
-#                     $COMMON
-#             done
 #             for step in "${eval_steps[@]}"; do
 #                 CUDA_VISIBLE_DEVICES=$cuda_id torchrun --nproc_per_node=1 --master_port=$MASTER_PORT \
-#                     eval.py \
+#                     eval2.py \
 #                     --config-name=tofu.yaml \
 #                     task_id=$task_id \
 #                     eval_unlearn_step=$step \
 #                     $COMMON
 #             done
-#             # for step in "${eval_steps[@]}"; do
-#             #     CUDA_VISIBLE_DEVICES=$cuda_id torchrun --nproc_per_node=1 --master_port=$MASTER_PORT \
-#             #         eval2.py \
-#             #         --config-name=tofu.yaml \
-#             #         task_id=$task_id \
-#             #         eval_unlearn_step=$step \
-#             #         $COMMON
-#             # done
 #         done
 #     done
 # done
