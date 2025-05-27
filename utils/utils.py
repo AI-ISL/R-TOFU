33be5307a24403b9d29bd5e8f42c4fdb9c5dafba
ssangyeon <136685661+ssangyeon@users.noreply.github.com>
2025-05-27 16:16:11 +0900

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -18,7 +18,7 @@ def get_model_identifiers_from_yaml(model_family):
             start_of_sequence_token: "<s>"
     '''
     model_configs = {}
-    with open("config/model_config.yaml", "r") as f:
+    with open("config/model_config.yaml", "r", encoding="utf-8) as f:
         model_configs = yaml.load(f, Loader=yaml.FullLoader)
     return model_configs[model_family]
 
